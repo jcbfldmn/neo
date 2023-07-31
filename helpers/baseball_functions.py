dffdb9c944453d40f8c4bc83fa40984431daf504
jcbfldmn <jakemfeldman13@gmail.com>
2023-07-30 23:47:55 -0400

    finalize weather related analysis

--- helpers/baseball_functions.py
+++ helpers/baseball_functions.py
@@ -7,3 +7,15 @@ def total_bases(hits, doubles, triples, homeruns):
     tb = singles + (doubles*2) + (triples*3) + (homeruns*4)
 
     return tb
+
+def batting_average(hits, at_bats):
+    ""
+    return round(sum(hits) / sum(at_bats), 3)
+
+def on_base_percentage(bases_reached, plate_appearances):
+    ""
+    return round(sum(bases_reached) / sum(plate_appearances), 3)
+
+def slugging_percentage(total_bases, at_bats):
+    ""
+    return round(sum(total_bases) / sum(at_bats), 3)
